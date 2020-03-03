b5e8e76efe9dfdbfe9944025210bf960ae51197a
Lanczi Dominik <nhcmon@gmail.com>
2020-03-03 11:38:08 +0100

    image change

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a second test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('Another Test Recipe', 'This is simply a second test', 'https://storage.needpix.com/rsynced_images/recipe-2508859_1280.jpg')
   ];
 
   constructor() { }
