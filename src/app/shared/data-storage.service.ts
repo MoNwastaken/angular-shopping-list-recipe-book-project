f35e1f9a8eb4e8ac8f344c6258086b26464ecd3e
Lanczi Dominik <nhcmon@gmail.com>
2020-03-11 16:03:22 +0100

    283 WORK IN PROGRESS

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,12 +2,13 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
-import {map} from "rxjs/operators";
+import {map, tap} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -17,12 +18,12 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://ng-course-recipe-book-4220a.firebaseio.com/recipes.json').pipe(map(recipes => {
+    return this.http.get<Recipe[]>('https://ng-course-recipe-book-4220a.firebaseio.com/recipes.json').pipe(map(recipes => {
       return recipes.map(recipe => {
         return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
       });
-    })).subscribe(recipes => {
+    }), tap(recipes => {
       this.recipeService.setRecipes(recipes);
-    });
+    }))
   }
 }
