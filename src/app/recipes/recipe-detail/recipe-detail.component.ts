dba164bba802a2a54e251144bfd12c529b382950
Lanczi Dominik <nhcmon@gmail.com>
2020-03-11 12:34:40 +0100

    233

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -31,4 +31,7 @@ export class RecipeDetailComponent implements OnInit {
     this.router.navigate(['edit'], {relativeTo: this.route})
   }
 
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+  }
 }
